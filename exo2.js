e58e7c85ce2eb141d57619b918e395495b870168
Audrey Hyvoz <audrey.hyvoz@gmail.comgit config --global user.email audrey.hyvoz@gmail.com>
2023-10-04 10:18:12 +0200

    fin exo2

---
+++ exo2.js
@@ -0,0 +1,12 @@
+<script>
+  document.getElementById("contactForm").addEventListener("submit", function(event) {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    if (name === "" || email === "" || message === "") {
+      alert("Veuillez remplir tous les champs du formulaire.");
+      event.preventDefault();
+    }
+  });
+</script>
